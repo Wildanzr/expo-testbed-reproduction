5b4a39f5a76276d6b358a15574d51dc12d126090
wildanzr <graita.sukma@gmail.com>
2024-04-04 07:46:20 +0700

    init gifted chat

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,13 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat, type IMessage } from "react-native-gifted-chat";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const ChatScreen = (): React.ReactNode => {
+const GiftedChatScreen = (): React.ReactNode => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello there!",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "Ana",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages: IMessage[]) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text className='text-white text-3xl font-bold'>ChatScreen</Text>
+    <SafeAreaView className="h-full w-full bg-primary-100">
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => {
+          onSend(messages);
+        }}
+        user={{
+          _id: 1,
+        }}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default ChatScreen
+export default GiftedChatScreen;
