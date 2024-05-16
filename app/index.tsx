b9da112632482f96f922903ffc6c31cb7ae28e6c
Danzrrr <graita.sukma@gmail.com>
2024-05-16 07:43:16 +0700

    moved

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,133 @@
-import { SafeAreaView, TouchableOpacity, View } from "react-native";
-import React, { useEffect, useRef } from "react";
+import { SafeAreaView, Text, View, Pressable, Platform } from "react-native";
+import React, { useRef, useState } from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { Link } from "expo-router";
-import { useChat } from "@/contexts/ChatContext";
+import { Audio } from "expo-av";
+import { StatusBar } from "expo-status-bar";
+import { getAudioApiCall } from "@/services/chatServices";
 
-export default function TabOneScreen() {
+export default function VoiceScreen() {
   const scrollRef = useRef<KeyboardAwareScrollView>(null);
-  const { fetchChats } = useChat();
+  const [recording, setRecording] = useState<Audio.Recording>();
+  const [audio, setAudio] = useState<string>("");
+  const [responseAudio, setResponseAudio] = useState<string>("");
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   fetchChats();
-  // }, [])
+  const startRecording = async () => {
+    try {
+      if (permissionResponse && permissionResponse.status !== "granted") {
+        console.log("Requesting permission..");
+        await requestPermission();
+      }
+      setAudio("");
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording..");
+      const recordingOptions = Platform.select({
+        android: Audio.RecordingOptionsPresets.HIGH_QUALITY,
+        ios: Audio.RecordingOptionsPresets.LOW_QUALITY,
+      });
+      const { recording } = await Audio.Recording.createAsync(recordingOptions);
+      setRecording(recording);
+      setResponseAudio("");
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  };
+
+  const stopRecording = async () => {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    if (recording) {
+      await recording.stopAndUnloadAsync();
+    }
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording?.getURI();
+    console.log("Recording stopped and stored at", uri);
+    setAudio(uri || "");
+  };
+
+  const playAudio = async (target: string) => {
+    const { sound } = await Audio.Sound.createAsync({ uri: target });
+    await sound.playAsync();
+  };
+
+  const generateAudio = async () => {
+    setResponseAudio("");
+    setLoading(true);
+    const { responseJSON, responseOk } = await getAudioApiCall(audio);
+
+    if (responseOk) {
+      console.log(responseJSON);
+      const audioUrl = responseJSON.data.content;
+      setResponseAudio(audioUrl);
+    }
+    setLoading(false);
+  };
 
   return (
-    <SafeAreaView className="flex-1 w-full h-full py-5">
+    <SafeAreaView className="flex-1 w-full h-full p-5 bg-primary-100">
+      <StatusBar style="dark" />
       <KeyboardAwareScrollView
         ref={scrollRef}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingVertical: 50 }}
         keyboardShouldPersistTaps="handled"
       >
-        <View className="flex-col items-center justify-center flex-1 space-y-5">
-          <Link href="/" className="text-xl font-bold text-blue-500">
-            HOME
-          </Link>
-          <Link href="/keyboard" className="text-xl font-bold text-blue-500">
-            KEYBOARD
-          </Link>
-          <Link href="/chat" className="text-xl font-bold text-blue-500">
-            CHAT
-          </Link>
-          <Link href="/voice" className="text-xl font-bold text-blue-500">
-            VOICE
-          </Link>
+        <View className="flex-col items-center justify-center w-full h-full space-y-10">
+          <Pressable
+            onPress={recording ? stopRecording : startRecording}
+            className="px-5 py-2 bg-red-500 rounded-lg"
+          >
+            <Text className="text-lg text-white font-jakarta-reg">
+              {recording ? "Recording.." : "Start Recording"}
+            </Text>
+          </Pressable>
+
+          {audio !== "" && (
+            <View className="flex flex-col items-center justify-center space-y-5">
+              <Pressable
+                style={{ marginHorizontal: 20 }}
+                onPress={() => playAudio(audio)}
+                className="px-5 py-2 rounded-lg bg-success-500"
+              >
+                <Text className="text-lg text-white font-jakarta-reg">
+                  Play
+                </Text>
+              </Pressable>
+
+              <Pressable
+                disabled={loading}
+                style={{ marginHorizontal: 20 }}
+                onPress={generateAudio}
+                className="px-5 py-2 rounded-lg bg-primary-500"
+              >
+                <Text className="text-lg text-white font-jakarta-reg">
+                  {loading ? "Loading.." : "Generate Audio"}
+                </Text>
+              </Pressable>
+            </View>
+          )}
+
+          {responseAudio !== "" && (
+            <View className="flex flex-col items-center justify-center space-y-5">
+              <Pressable
+                style={{ marginHorizontal: 20 }}
+                onPress={() => playAudio(responseAudio)}
+                className="px-5 py-2 rounded-lg bg-success-500"
+              >
+                <Text className="text-lg text-white font-jakarta-reg">
+                  Play
+                </Text>
+              </Pressable>
+            </View>
+          )}
         </View>
       </KeyboardAwareScrollView>
     </SafeAreaView>
